aeacc2db0d050e8649b6f8457fc24473e3278a2b
Salomao-Faleta <faletas41@gmail.com>
2023-12-19 21:45:06 -0300

    Parte 3 - Criando a seção dos poemas
    Falta acrescentar mais coisas e fazer a seção lembrei de você.

---
+++ src/script/poesias.js
@@ -0,0 +1,9 @@
+document.getElementById('subir').addEventListener('click', ()=>{
+    setTimeout(()=>{
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    }, 200)
+})
