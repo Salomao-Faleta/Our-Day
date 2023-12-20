5b8964eb55b5226245ccf48b4d8a081430984012
Salomao-Faleta <faletas41@gmail.com>
2023-12-19 22:10:43 -0300

    parte 3 - Aperfeiçoando

--- src/script/poesias.js
+++ src/script/poesias.js
@@ -6,4 +6,17 @@ document.getElementById('subir').addEventListener('click', ()=>{
             behavior: "smooth"
         });
     }, 200)
-})
+});
+
+
+function subirTela(){
+    if(window.scrollY > 500){
+        document.getElementById('subir').style.display = 'block';
+    }else{
+        document.getElementById('subir').style.display = 'none';
+    }
+
+}
+
+
+addEventListener('scroll', subirTela);
